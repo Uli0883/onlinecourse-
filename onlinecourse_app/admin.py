db1d1c4b94a5d62b160d2847700cf81ab7c77d14
Ulises Carrillo <156877334+Uli0883@users.noreply.github.com>
2026-07-06 21:44:17 +0000

    Commit final

--- onlinecourse_app/admin.py
+++ onlinecourse_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Instructor, Learner, Course, Lesson, Question, Choice, Submission
-
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 # Inlines
 class LessonInline(admin.TabularInline):
     model = Lesson
